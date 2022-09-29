4d5ccbff3164a0b130a695b55bb3630045c14dea
Cristian Medina <medinacristian@hotmail.com>
2022-09-29 09:05:45 -0700

    Added assignment cloud with puppet

---
+++ Assignments/06. Provisioning and Deployment on the Cloud/manifests/default.pp
@@ -0,0 +1,42 @@
+# Execute apt-get update
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+# AppServer
+class app {
+  # Install npm
+  package { 'npm':
+    require => Exec['apt-update'], 
+    ensure => 'installed'
+  }
+
+  # Install Nodejs
+  package { 'nodejs':
+    require => Exec['apt-update'], 
+    ensure => 'installed'
+  }
+}
+
+# DBServer
+class db {
+  # Install mysql-server
+  package { 'mysql-server':
+    require => Exec['apt-update'], 
+    ensure => 'installed'
+  }
+
+  # Run mysql
+  service { 'mysql':
+    ensure  => running,
+    require => Package['mysql-server']
+  }
+}
+
+node 'appserver' {
+  class { 'app': }
+}
+
+node 'dbserver' {
+  class { 'db': }
+}
